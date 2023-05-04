cb8e99db5bcfe112686f58afe7b972fd83efcd90
Likhith Nemani <likhith.nemani@sjsu.edu>
2023-05-03 17:00:38 -0700

    Added activities and fragments

--- app/src/main/java/edu/sjsu/android/expensetracker/ShowExpense.java
+++ app/src/main/java/edu/sjsu/android/expensetracker/ShowExpense.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class ShowExpense extends AppCompatActivity {
 
     TextView amountID, categoryID, dateID, noteID;
