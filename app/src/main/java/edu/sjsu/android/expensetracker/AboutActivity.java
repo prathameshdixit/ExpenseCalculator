acc700115c0799ed3f8a5b4859f24dcc2b99713d
Prathamesh Dixit <prathamesh.dixit@sjsu.edu>
2023-05-04 21:14:43 -0700

    about and help updated

--- app/src/main/java/edu/sjsu/android/expensetracker/AboutActivity.java
+++ app/src/main/java/edu/sjsu/android/expensetracker/AboutActivity.java
@@ -1,14 +1,18 @@
 package edu.sjsu.android.expensetracker;
 
 
+import android.content.ClipData;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import java.util.ArrayList;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -17,6 +21,21 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         applyTheme();
         setContentView(R.layout.about_activity);
+        ImageView icon1 = findViewById(R.id.icon1);
+        icon1.setImageResource(R.drawable.img1);
+
+        ImageView icon2 = findViewById(R.id.icon2);
+        icon2.setImageResource(R.drawable.img2);
+
+        ImageView icon3 = findViewById(R.id.icon3);
+        icon3.setImageResource(R.drawable.img3);
+
+        ImageView icon4 = findViewById(R.id.icon4);
+        icon4.setImageResource(R.drawable.img4);
+       
+        ImageView icon5 = findViewById(R.id.icon5);
+        icon5.setImageResource(R.drawable.img5);
+
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -24,9 +43,12 @@ public class AboutActivity extends AppCompatActivity {
             getSupportActionBar().setTitle(getString(R.string.about));
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+            }
         }
 
-    }
+
     private void applyTheme() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this );
         int oldTheme = Integer.parseInt(sharedPreferences.getString("Theme", "0"));
